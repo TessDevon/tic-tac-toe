19ab50f54301978aec0360e64a212eafe99bbf22
Therese <therese.holen22@gmail.com>
2023-05-29 16:30:44 +0200

    Added StartVue

---
+++ src/components/Play.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const playerX = "";
+const playerO = "";
+
+const addPlayers = (texts: string[]) => {
+    //Lägga in värdet för spelarna. 
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Start.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref} from 'vue'
+
+const userInputX = ref("");
+const userInputO = ref("");
+
+const emits = defineEmits<{ (e:"addPlayers", texts: string[]): void}>();
+
+const handleSubmit = () => {
+    emits("addPlayers", [userInputO.value, userInputX.value] );
+    userInputO.value="";
+    userInputX.value="";
+}
+
+</script>
+
+<template>
+    <h2>Registrera spelare</h2>
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <input placeholder="Player O" type="text" v-model="userInputO" id="O">
+            <input placeholder="Player X" type="text" v-model="userInputX" id="X">
+        </form>
+    </div>
+    <button></button>
+</template>
+
+<style scoped>
+</style>
