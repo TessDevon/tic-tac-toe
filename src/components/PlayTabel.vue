d91196ea4e5f5d9eeabdd62f3d56f8854dfe14d3
Therese <therese.holen22@gmail.com>
2023-05-31 13:17:25 +0200

    Child to playarea.

--- src/components/PlayTabel.vue
+++ src/components/PlayTabel.vue
@@ -1,53 +1,39 @@
 <script setup lang="ts">
+import { PlaySquare } from "../Models/IGameTabel";
+//import { ShowPlayPlan} from "../components/Play.vue"
+//import { props } from 'vue';
 //import { GameTabel } from '../Models/IGameTabel';
 
 
-const GameTabelPlan = [{symbol:"", id:0 },{symbol:"", id:1},{symbol:"", id:2},{symbol:"", id:3},{symbol:"", id:4},{symbol:"", id:5},{symbol:"", id:6},{symbol:"", id:7},{symbol:"", id:8}];
-console.log(GameTabelPlan);
-//GameTabelPlan = GameTabelPlan.values.map((GameTabelPlan) => {
-//});
+/*const GameTabelPlan = [{symbol:"", id:0 },{symbol:"", id:1},{symbol:"", id:2},{symbol:"", id:3},{symbol:"", id:4},{symbol:"", id:5},{symbol:"", id:6},{symbol:"", id:7},{symbol:"", id:8}];
+console.log(GameTabelPlan);*/
 
+interface ShowPlayPlanProps {
+    playSquare:PlaySquare; 
+};
+
+const props = defineProps<ShowPlayPlanProps>();
 
 </script>
 
 <template>
-    <div>
-    <h1>Tre i rad</h1>
-    <p></p><!--Hämta upp andändaren och meddela att det är dennes tur-->    
-        <div>
-            <ul class="playContainer">
-                <li class="playPart" v-for="(symbol, value) in GameTabelPlan">
-                {{ symbol }}</li>
-            </ul>
-        </div>
+    <div class="playPart">
+    {{ props.playSquare.symbol }}
     </div>
 </template>
 
 <style scoped>
 
-.playContainer {
-    width: 450px;
-    height: 450px;
-    display: grid;
-    grid-template-columns: 150px 150px 150px;
-    grid-template-rows: 150px 150px 150px;
-    justify-items: center;
-    align-items: center;
-    /*
-    flex-direction: row;
-    margin: 0px;
-    padding: 0px;
-    */
-}
-
 .playPart {
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 20px;
+    font-size: 20px;                        /*Större storlek när jag inte längre vill se all info i blocken.*/ 
     height: 100%;
     width: 100%;
-    border: 1px solid black;
+    border: 3px solid black;
+    border-radius: 6px;
+    cursor: pointer;
 }
 
 .playPart:hover{
